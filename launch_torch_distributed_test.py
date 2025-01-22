c8a9ff4cf80899900666985144b5a5cecc639239
YitaoYuan <yuanyitao@pku.edu.cn>
2025-01-22 16:53:04 +0800

    edit comment

--- launch_torch_distributed_test.py
+++ launch_torch_distributed_test.py
@@ -7,31 +7,31 @@ import os
 import subprocess
 
 def get_pci_device(device_path):
-    """获取设备对应的 PCI 设备路径"""
+    """Get the PCI device path for a given device."""
     device_link = os.path.join(device_path, 'device')
     if os.path.islink(device_link):
         return os.path.realpath(device_link)
     return None
 
 def get_device_mappings():
-    """获取 IB 网卡和以太网卡的映射关系"""
+    """Get mappings between IB devices and Ethernet devices."""
     ib_to_eth = {}
     eth_to_ib = {}
     ib_devices_path = '/sys/class/infiniband'
     net_devices_path = '/sys/class/net'
 
-    # 遍历所有 IB 设备
+    # Traverse all IB devices
     for ib_device in os.listdir(ib_devices_path):
         ib_device_path = os.path.join(ib_devices_path, ib_device)
         ib_pci_device = get_pci_device(ib_device_path)
 
         if ib_pci_device:
-            # 遍历所有以太网设备
+            # Traverse all Ethernet devices
             for eth_device in os.listdir(net_devices_path):
                 eth_device_path = os.path.join(net_devices_path, eth_device)
                 eth_pci_device = get_pci_device(eth_device_path)
 
-                # 如果 PCI 设备匹配，则建立映射
+                # If PCI devices match, create a mapping
                 if eth_pci_device and eth_pci_device == ib_pci_device:
                     ib_to_eth[ib_device] = eth_device
                     eth_to_ib[eth_device] = ib_device
@@ -40,12 +40,12 @@ def get_device_mappings():
     return ib_to_eth, eth_to_ib
 
 def ib_to_eth(ib_device):
-    """将 IB 网卡名转换为以太网卡名"""
+    """Convert an IB device name to an Ethernet device name."""
     ib_to_eth_mapping, _ = get_device_mappings()
     return ib_to_eth_mapping.get(ib_device, "Unknown IB device")
 
 def eth_to_ib(eth_device):
-    """将以太网卡名转换为 IB 网卡名"""
+    """Convert an Ethernet device name to an IB device name."""
     _, eth_to_ib_mapping = get_device_mappings()
     return eth_to_ib_mapping.get(eth_device, "Unknown Ethernet device")
 
